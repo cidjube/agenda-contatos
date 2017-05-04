644c87c7255bab3de2e1d62eb9d2bc3a897c55ef
Cid <cid@tjgo.jus.br>
2017-05-04 13:08:28 -0300

    commit inicial

---
+++ app/src/main/java/com/example/agenda/agendacontatos/ActContatos.java
@@ -0,0 +1,13 @@
+package com.example.agenda.agendacontatos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActContatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_contatos);
+    }
+}
